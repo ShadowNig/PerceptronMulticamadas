33e0a29935710d0a3f0e6285380aff3e923525f6
Maqueise <45274111+ShadowNig@users.noreply.github.com>
2021-08-18 16:20:22 -0300

    Add files via upload

---
+++ mushrooms/mushrooms_modelos_(plot_acuracia).R
@@ -0,0 +1,37 @@
+source("MLP_Maqueise_(plot_acuracia).R", encoding = "UTF-8")
+
+treino = read.csv("mushrooms_treino.csv")
+teste  = read.csv("mushrooms_teste.csv")
+
+x.treino = treino[1:100,-1]
+y.treino = as.matrix(treino[1:100,1])
+
+# modelo 7
+set.seed(92021)
+mod = perceptron.multicamadas(treino=x.treino,  
+                              esperado=y.treino,
+                              cam.oculta = c(3,2),
+                              max.iteracoes = 10000,
+                              pc=0.4991)
+
+saveRDS(mod, "acc mod7.rds")
+
+# modelo 13
+set.seed(92021)
+mod = perceptron.multicamadas(treino=x.treino,  
+                              esperado=y.treino,
+                              cam.oculta = c(2,2,2,2,2),
+                              max.iteracoes = 10000,
+                              pc=0.5085)
+
+saveRDS(mod, "acc mod13.rds")
+
+# modelo 17
+set.seed(92021)
+mod = perceptron.multicamadas(treino=x.treino,  
+                              esperado=y.treino,
+                              cam.oculta = c(3,3,3,3,3,3,3,3,3,3) ,
+                              max.iteracoes = 10000,
+                              pc=0.5302)
+
+saveRDS(mod, "acc mod17.rds")
